10d5ed7469359b9ae720af3bc28eb9232d3c8fda
billie <tech.billie@gmail.com>
2020-12-17 17:20:05 +0900

    추천도서 읽을 책에 추가하기 기능 구현

--- app/src/main/java/com/example/everybooks/RecommendBookAdapter.java
+++ app/src/main/java/com/example/everybooks/RecommendBookAdapter.java
@@ -3,6 +3,11 @@ package com.example.everybooks;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,16 +20,26 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.everybooks.data.Book;
 import com.example.everybooks.data.RecommendBook;
+import com.example.everybooks.util.Util;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class RecommendBookAdapter extends RecyclerView.Adapter<RecommendBookAdapter.BookViewHolder>
 {
     private int position;
-    Book book;
+    RecommendBook recommendBook;
 
     Context context;
+    int bookId;
+    String bookListString;
+    JSONArray jsonArray;
+    Intent intent;
 
     static ArrayList<RecommendBook> recommendBookList = new ArrayList<>();
 
@@ -58,8 +73,103 @@ public class RecommendBookAdapter extends RecyclerView.Adapter<RecommendBookAdap
                             {
                                 // 읽을 책 리스트에 추가한다.
                                 position = getAdapterPosition();
-                                //Book book = getItem(position);
+                                recommendBook = getItem(position);
+
                                 //addItem(book);
+
+                                // ImageView 의 이미지를 SharedPreference 에 저장하기 위해서
+                                // ImageView 의 resource 를 Bitmap 으로 가져온 뒤
+                                BitmapDrawable drawable = (BitmapDrawable) imageView_img.getDrawable();
+                                Bitmap bitmap = drawable.getBitmap();
+
+                                // Bitmap 을 문자열로 바꿔서 이미지를 문자열 변수에 저장한다.
+                                Util util = new Util();
+                                String imgString = util.bitMapToString(bitmap);
+
+                                // 책들을 구분하는 값을 저장하기 위해서 bookId 를 사용한다.
+                                // bookInfo 라는 SharedPreferences 파일에서 bookId를 가져온다.
+                                // 저장된 값이 존재하지 않는다면 0을 가져온다.
+                                SharedPreferences bookInfo = context.getSharedPreferences("bookInfo", MODE_PRIVATE);
+                                bookId = bookInfo.getInt("bookId", 0);
+
+                                // 아이템의 정보를 book 객체에 저장한 뒤 JsonArray 에 저장하기 위해서 jsonObject 형태로 바꾼다.
+                                Book book = new Book();
+                                book.setBookId(bookId);
+                                book.setImg(imgString);
+                                book.setTitle(recommendBook.getTitle());
+                                book.setWriter(recommendBook.getWriter());
+                                book.setPublisher(recommendBook.getPublisher());
+                                book.setPublishDate(recommendBook.getPublishDate());
+                                book.setState("toRead");
+
+                                Calendar cal = Calendar.getInstance();
+                                int year = cal.get ( cal.YEAR );
+                                int month = cal.get ( cal.MONTH ) + 1 ;
+                                int date = cal.get ( cal.DATE ) ;
+
+                                String today = year + "." + month + "." + date;
+                                book.setInsertDate(today);
+                                book.setStartDate("");
+                                book.setEndDate("");
+                                book.setReadTime("00:00:00");
+                                book.setStarNum(0);
+
+                                try
+                                {
+                                    JSONObject bookJson = new JSONObject();
+
+                                    bookJson.put("bookId", book.getBookId());
+                                    bookJson.put("img", book.getImg());
+                                    bookJson.put("title", book.getTitle());
+                                    bookJson.put("writer", book.getWriter());
+                                    bookJson.put("publisher", book.getPublisher());
+                                    bookJson.put("publishDate", book.getPublishDate());
+                                    bookJson.put("state", book.getState());
+                                    bookJson.put("insertDate", book.getInsertDate());
+                                    bookJson.put("startDate", book.getStartDate());
+                                    bookJson.put("endDate", book.getEndDate());
+                                    bookJson.put("readTime", book.getReadTime());
+                                    bookJson.put("starNum", book.getStarNum());
+
+                                    // 책을 구분하기 위해 저장된 책의 bookId 가 겹치지 않도록 bookInfo 에 저장된 bookId의 값을 1 증가시킨다.
+                                    SharedPreferences.Editor editor = bookInfo.edit();
+                                    editor.putInt("bookId", bookId + 1);
+                                    editor.commit();
+
+                                    // SharedPreference bookInfo 파일에서 "toReadBookLIst" 키로 저장된 책 리스트를 불러온다.
+                                    bookListString = bookInfo.getString("bookList", null);
+
+                                    // 저장된 책 리스트가 있다면 문자열을 JsonArray 형식으로 바꾼 뒤
+                                    // 사용자의 입력값을 저장해둔 jsonObject 를 추가한다.
+                                    if(bookListString != null)
+                                    {
+                                        jsonArray = new JSONArray(bookListString);
+                                        jsonArray.put(bookJson);
+                                        bookListString = jsonArray.toString();
+                                        editor.putString("bookList", bookListString);
+                                        editor.commit();
+                                    }
+                                    else
+                                    {
+                                        // 저장된 책 리스트가 없다면 JsonArray 를 만들어서
+                                        // 사용자의 입력값을 저장해둔 jsonObject 를 추가한다.
+                                        jsonArray = new JSONArray();
+                                        jsonArray.put(bookJson);
+                                        bookListString = jsonArray.toString();
+                                        editor.putString("bookList", bookListString);
+                                        editor.commit();
+                                    }
+
+                                    Log.d("recommendBookAdapter", " bookListString : " + bookListString);
+
+                                }
+                                catch (Exception e)
+                                {
+                                    System.out.println(e.toString());
+                                }
+
+                                intent = new Intent(context, MainActivity.class);
+                                context.startActivity(intent);
 
                                 dialog.dismiss();
 
@@ -130,7 +240,7 @@ public class RecommendBookAdapter extends RecyclerView.Adapter<RecommendBookAdap
     }
 
     // 아이템 가져오는 메소드
-    public Book getItem(int position) {
+    public RecommendBook getItem(int position) {
         return recommendBookList.get(position);
     }
 
