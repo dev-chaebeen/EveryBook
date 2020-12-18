d046b2b67ed7aa7817e705c628a801631f02ba67
billie <tech.billie@gmail.com>
2020-12-18 18:51:20 +0900

    MPAndroid Chart 적용

--- app/src/main/java/com/example/everybooks/ChartFragment.java
+++ app/src/main/java/com/example/everybooks/ChartFragment.java
@@ -1,7 +1,10 @@
-package com.example.everybooks;
+ package com.example.everybooks;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +18,10 @@ import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class ChartFragment extends Fragment
@@ -23,6 +30,21 @@ public class ChartFragment extends Fragment
 
     // 뷰 요소 선언
     BarChart chart;
+
+    int January;
+    int February;
+    int March;
+    int April;
+    int May;
+    int June;
+    int July;
+    int August;
+    int September;
+    int October;
+    int November;
+    int December;
+
+    final String TAG = "ChartFragment";
 
     @Nullable
     @Override
@@ -62,22 +84,98 @@ public class ChartFragment extends Fragment
         month.add("11월");
         month.add("12월");
 
+        // 저장되어있는 데이터 불러오기
+        try
+        {
+            SharedPreferences bookInfo = view.getContext().getSharedPreferences("bookInfo", Context.MODE_PRIVATE);
+            String bookListString = bookInfo.getString("bookList", null);
+
+            //Log.d(TAG, "bookListString : " + bookListString);
+            if(bookListString != null)
+            {
+                JSONArray jsonArray = new JSONArray(bookListString);
+                for (int i = 0; i < jsonArray.length(); i++)
+                {
+                    JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+
+                    if(jsonObject.getString("state").equals("read"))
+                    {
+                        String endDate = jsonObject.getString("endDate");
+
+                        Log.d(TAG, "endDate : " + endDate);
+
+                        String[] yearMonthDate = endDate.split("\\.");
+
+                        if (yearMonthDate[0].equals("2020")) {
+                            switch (yearMonthDate[1]) {
+                                case "01":
+                                    January++;
+                                    break;
+                                case "02":
+                                    February++;
+                                    break;
+                                case "03":
+                                    March++;
+                                    break;
+                                case "04":
+                                    April++;
+                                    break;
+                                case "05":
+                                    May++;
+                                    break;
+                                case "06":
+                                    June++;
+                                    break;
+                                case "07":
+                                    July++;
+                                    break;
+                                case "08":
+                                    August++;
+                                    break;
+                                case "09":
+                                    September++;
+                                    break;
+                                case "10":
+                                    October++;
+                                    break;
+                                case "11":
+                                    November++;
+                                    break;
+                                case "12":
+                                    December++;
+                                    break;
+
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        catch (JSONException e)
+        {
+            System.out.println(e.toString());
+        }
+
+
+
+
         // 차트에 출력할 독서 권수
         ArrayList NumOfBooks = new ArrayList();
 
         // 월별 독서량
-        NumOfBooks.add(new BarEntry(1, 0));
-        NumOfBooks.add(new BarEntry(2, 1));
-        NumOfBooks.add(new BarEntry(3, 2));
-        NumOfBooks.add(new BarEntry(4, 3));
-        NumOfBooks.add(new BarEntry(5, 4));
-        NumOfBooks.add(new BarEntry(6, 5));
-        NumOfBooks.add(new BarEntry(1, 6));
-        NumOfBooks.add(new BarEntry(4, 7));
-        NumOfBooks.add(new BarEntry(5, 8));
-        NumOfBooks.add(new BarEntry(6, 9));
-        NumOfBooks.add(new BarEntry(1, 10));
-        NumOfBooks.add(new BarEntry(29, 11));
+        NumOfBooks.add(new BarEntry(January, 0));
+        NumOfBooks.add(new BarEntry(February, 1));
+        NumOfBooks.add(new BarEntry(March, 2));
+        NumOfBooks.add(new BarEntry(April, 3));
+        NumOfBooks.add(new BarEntry(May, 4));
+        NumOfBooks.add(new BarEntry(June, 5));
+        NumOfBooks.add(new BarEntry(July, 6));
+        NumOfBooks.add(new BarEntry(August, 7));
+        NumOfBooks.add(new BarEntry(September, 8));
+        NumOfBooks.add(new BarEntry(October, 9));
+        NumOfBooks.add(new BarEntry(November, 10));
+        NumOfBooks.add(new BarEntry(December, 11));
 
         // 데이터 담기
         BarDataSet bardataset = new BarDataSet(NumOfBooks, "Number Of Books");
@@ -103,5 +201,7 @@ public class ChartFragment extends Fragment
 
         // 차트에 데이터 넣기
         chart.setData(data);
+
+        Log.d(TAG, "12월에 읽은 책 수 : " + December );
     }
 }
--- app/src/main/java/com/example/everybooks/TranslateActivity.java
+++ app/src/main/java/com/example/everybooks/TranslateActivity.java
@@ -1,12 +1,50 @@
 package com.example.everybooks;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class TranslateActivity extends AppCompatActivity
 {
@@ -16,6 +54,14 @@ public class TranslateActivity extends AppCompatActivity
     EditText editText_result;       // 번역 결과
 
     Intent intent;
+    String TAG = "TranslateActivity";
+
+
+    final int TAKE_PICTURE = 1000;
+    final int OPEN_GALLERY = 1001;
+
+    private TessBaseAPI m_Tess; //Tess API reference
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -29,5 +75,290 @@ public class TranslateActivity extends AppCompatActivity
         imageView_add_photo = findViewById(R.id.add_photo);
         button_translate = findViewById(R.id.btn_translate);
         editText_result = findViewById(R.id.result);
+
+        // 각 요소를 클릭하면 수행할 동작 지정해두기
+        View.OnClickListener click = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.add_photo:
+                        // 책 추가 이미지 클릭하면 팝업 메뉴를 띄운다.
+                        // 1. 카메라  2.갤러리  3.기본이미지
+                        PopupMenu popupMenu = new PopupMenu(getApplicationContext(), v);
+                        getMenuInflater().inflate(R.menu.img_menu, popupMenu.getMenu());
+
+                        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+                        {
+                            @Override
+                            public boolean onMenuItemClick(MenuItem menuItem) {
+
+                                switch(menuItem.getItemId())
+                                {
+                                    case R.id.camera :
+                                        //openCamera();
+                                        dispatchTakePictureIntent();
+                                        break;
+
+                                    case R.id.gallery :
+                                        openGallery();
+                                        break;
+
+                                    case R.id.basic :
+                                        imageView_add_photo.setImageResource(R.drawable.eng_book);
+                                        break;
+                                }
+                                return true;
+                            }
+                        });
+
+                        popupMenu.show();// 팝업 메뉴 보이기
+
+                        break;
+
+
+                    case R.id.btn_translate :
+                        break;
+                }
+            }
+        };
+
+        // 각 요소가 클릭되면
+        imageView_add_photo.setOnClickListener(click);
+        button_translate.setOnClickListener(click);
+
+
+    }
+
+    // 갤러리 실행 메소드
+    public void openGallery()
+    {
+        // 권한 체크
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted()
+            {
+                // 권한 허가 상태일 때
+                Toast.makeText(getApplicationContext(), "권한 허가",Toast.LENGTH_SHORT).show();
+
+                // 갤러리 열기
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Get Album"), OPEN_GALLERY);
+
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions)
+            {
+                // 권한 허가하지 않았을 때
+                Toast.makeText(getApplicationContext(), "권한을 허용하지 않으면 서비스를 이용할 수 없습니다.",Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        // 권한 체크
+        TedPermission.with(getApplicationContext())
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage(" 갤러리 접근 권한이 필요합니다.")
+                .setDeniedMessage(" [설정] > [권한] 에서 권한을 허용할 수 있습니다.")
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        Log.d(TAG,"onActivityResult 시작");
+        // 암묵적 인텐트 응답 결과에 따라 동작을 실행한다.
+        // 카메라로 찍은 사진, 갤러리에서 불러온 사진을 이미지뷰에 보여준다.
+        switch (requestCode)
+        {
+            case TAKE_PICTURE:
+                // 카메라로 찍은 사진을 받는다.
+                if(resultCode == RESULT_OK)
+                {
+
+                    if (resultCode == RESULT_OK && intent.hasExtra("data") )
+                    {
+                        Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
+                        if (bitmap != null)
+                        {
+                            imageView_add_photo.setImageBitmap(bitmap);
+                        }
+                    }
+                }
+
+                break;
+
+                case OPEN_GALLERY:
+                if(requestCode == OPEN_GALLERY && resultCode == RESULT_OK)
+                {
+                    try
+                    {
+                        InputStream is = getContentResolver().openInputStream(intent.getData());
+                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        is.close();
+                        imageView_add_photo.setImageBitmap(bitmap);
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                }
+                else if(requestCode == OPEN_GALLERY && resultCode == RESULT_CANCELED)
+                {
+                    Toast.makeText(this,"선택 취소", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResult) {
+        if (requestCode == 0) {
+
+        } else {
+
+        }
+    }
+
+    // 이미지를 원본과 같게 회전시킨다.
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+
+
+    /*private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  *//* prefix *//*
+                ".jpg",         *//* suffix *//*
+                storageDir      *//* directory *//*
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }*/
+
+    /**
+     * 기본카메라앱을 실행 시킨다.
+     */
+   /* private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // 사진파일을 생성한다.
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // 사진파일이 정상적으로 생성되었을때
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        this.getApplicationContext().getPackageName()+".fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, ACT_TAKE_PIC);
+            }
+        }
+    }
+*/
+    // test 사진 촬영
+    private void dispatchTakePictureIntent()
+    {
+        // 권한 체크
+        PermissionListener permissionListener = new PermissionListener()
+        {
+            @Override
+            public void onPermissionGranted()
+            {
+                // 권한 허가 상태일 때
+                Toast.makeText(getApplicationContext(), "권한 허가",Toast.LENGTH_SHORT).show();
+
+                // 카메라 열기
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, TAKE_PICTURE);
+
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+                    // 파일을 생성한다
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try
+                    {
+                        photoFile = createImageFile();
+                    }
+                    catch (IOException ex)
+                    {
+                        System.out.println(ex.toString());
+                    }
+
+                    // 파일이 생성되면
+                    if (photoFile != null)
+                    {
+                        Uri photoURI = FileProvider.getUriForFile(getApplicationContext(),
+                                "com.example.everybooks",
+                                photoFile);
+
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, TAKE_PICTURE);
+                    }
+                }
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions)
+            {
+                // 권한 허가하지 않았을 때
+                Toast.makeText(getApplicationContext(), "권한을 허용하지 않으면 서비스를 이용할 수 없습니다.",Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        // 권한 체크
+        TedPermission.with(getApplicationContext())
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage(" 카메라 접근 권한이 필요합니다")
+                .setDeniedMessage(" [설정] > [권한] 에서 권한을 허용할 수 있습니다.")
+                .setPermissions(Manifest.permission.CAMERA)
+                .check();
+    }
+
+    // 사진 저장될 경로
+    String currentPhotoPath;
+
+
+    //사진을 파일로 만드는 메소드
+    private File createImageFile() throws IOException {
+
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES); // 그림 파일 저장 (/mnt/sdcard/Pictures)
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
